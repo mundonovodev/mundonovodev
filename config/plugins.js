4cf17cc8db315423208f8406e95d3cf46957a15b
Euclides Catunda <euclides.catunda@b2wdigital.com>
2021-09-14 10:22:28 -0300

    Arrumando as variáveis de ambiente

--- config/plugins.js
+++ config/plugins.js
@@ -6,8 +6,9 @@ module.exports = ({ env }) => ({
       apiKey: env('SENDGRID_API_KEY'),
     },
     settings: {
-      defaultFrom: 'mundonovodev@gmail.com',
-      defaultReplyTo: 'mundonovodev@gmail.com',
+      defaultFrom: env('DEFAULT_FROM'),
+      defaultReplyTo: env('DEFAULT_REPLY_TO'),
+      testAddress: env('TEST_ADDRESS'),
     },
   },
   // ...
